156f9162f5883718165ff272631ad8a3538c36d9
RyoyaBamba <ryoya.bamba@gmail.com>
2025-06-23 16:39:08 +0900

    modified variables in train_model.py

--- train_model.py
+++ train_model.py
@@ -13,10 +13,10 @@ os.makedirs("demand_prediction", exist_ok=True)
 
 # データ読み込み
 weather = pd.read_csv("open-meteo-35.68N139.81E6m.csv", index_col=0,parse_dates=True)
-dataset = pd.read_csv("beers.csv", index_col=0,parse_dates=True)
+beer_dataset = pd.read_csv("beers.csv", index_col=0,parse_dates=True)
 
 # 結合・前処理
-df = pd.merge(dataset, weather, left_index=True, right_index=True)
+df = pd.merge(beer_dataset, weather, left_index=True, right_index=True)
 df = df.fillna(0)
 #金曜日かどうかの列を追加
 df.index = pd.to_datetime(df.index)
@@ -55,7 +55,7 @@ base_model = DecisionTreeRegressor(max_depth=5, random_state=0)
 multi_model = MultiOutputRegressor(base_model)
 multi_model.fit(X_train, y_train)
 
-"""
+"""（消去）
 # 予測
 y_pred = multi_model.predict(X_test)
 # 評価表示
